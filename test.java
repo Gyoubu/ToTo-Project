153ceb5b61d3c65efaa1ac4c3e2e1735543483ce
Gyoubu <iischance@yahoo.co.jp>
2017-03-25 16:01:18 +0900

    test
    fast test

---
+++ test.java
@@ -0,0 +1,16 @@
+public void main(String args) {
+
+	String 欲しいもの = getFirstString(args);
+
+	SytemPrintOut 欲しいもの;
+}
+
+private String getFirstString(String 引数1){
+	string 戻り値 = なんかよこせ
+	if (引数1 == らぐへい) {
+		戻り値 = 引数1 + うんこ野郎;
+	} else if (ぷーちゃん) {
+		戻り値 = 引数1 + SMITEやろうぜ！;
+	}
+	return 戻り値;
+}
